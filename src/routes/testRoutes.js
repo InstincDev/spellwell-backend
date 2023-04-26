7b0da30ac9e99569bb61461272952b7eed4fad23
InstincDev <crene7722@gmail.com>
2023-04-26 12:18:18 -0500

    removed old comments and out-dated code, added error message response, and test GET now returns json

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -1,38 +1,30 @@
 import { Router } from "express";
-import {connectDB} from "../utils/connectDB.js";
-import Test from "../models/testModels.js"
-
+import { connectDB } from "../utils/connectDB.js";
+import Test from "../models/testModels.js";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
     try {
-        const {baseWords, challengeWords} = req.body
-        // check if base and challenge words are undefined
-        if (baseWords == undefined || challengeWords == undefined) throw Error('Base and or Challenge Words undefined')  
-        // check if base and challenge words length > 0 
-        if (baseWords.length <= 0 || challengeWords.length <= 0) throw Error('Insufficient Words Provided')    
-
-       await connectDB(); 
-        const test = await Test.create(req.body)
-        await test.save()
+        await connectDB();
+        const test = await Test.create(req.body);
+        await test.save();
         res.status(200).json(test);
     } catch (error) {
         console.log(error.message);
-        res.status(400).send();
+        res.status(400).send(error.message);
     }
 });
 
-router.get("/", async (req,res) =>{
+router.get("/", async (req, res) => {
     try {
         await connectDB();
-       const test = await Test.find() 
-       console.log(test);
-        res.status(200).send()
+        const tests = await Test.find();
+        res.status(200).json(tests);
     } catch (error) {
-       console.log(error.message);
-       res.send(error.message).status(400) 
+        console.log(error.message);
+        res.send(error.message).status(400);
     }
-})
+});
 
-export default router
+export default router;
